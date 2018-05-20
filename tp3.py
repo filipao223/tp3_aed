a5a1948743992aa949273c37be333d0bf5a5a6ee
filipao223 <jpmontenegro67@gmail.com>
2018-05-20 14:57:33 +0100

    Print da tabela

--- tp3.py
+++ tp3.py
@@ -5,10 +5,9 @@ alf = 'abcdefghijklmnopqrstuvwxyz'
 
 class Node(object):
     """docstring for Node."""
-    def __init__(self, palavra, freq):
+    def __init__(self, palavra):
         super(Node, self).__init__()
         self.palavra = palavra
-        self.frequencia = freq
         self.nextNode = None
 
 
@@ -17,12 +16,13 @@ class Lista(object):
     def __init__(self):
         super(Lista, self).__init__()
         self.head = Node("")
+        self.numero = 0
 
     def add(self,palavra):
-        current = self.head.nextNode
-
-        if current is None:
-            current = Node(palavra)
+        current = self.head
+        if current.nextNode is None:
+            current.nextNode = Node(palavra)
+            self.numero+=1
             return True
         else:
             current = current.nextNode
@@ -31,14 +31,19 @@ class Lista(object):
 
     def get(self, palavra):
         current = self.head.nextNode
-
         if current is None:
             return ""
         else:
-            while(current != None):
-                if(current.palavra == palavra): return palavra
+            while current is not None:
+                if current.palavra == palavra: return palavra
                 current = current.nextNode
             return ""
+
+    def printLista(self,maxList, head, i):
+        if head is not None and i!=maxList:
+            i+=1
+            return head.palavra+"-->"+self.printLista(maxList, head.nextNode, i)
+        else: return ""
 
 
 class Tabela(object):
@@ -65,6 +70,14 @@ class Tabela(object):
 
         return ""
 
+    def printHash(self,noEmpty, maxHashT, maxList):
+        for i in range(self.size):
+            if self.listas[i] is None :
+                print("lista["+str(i)+"]: None")
+            else:
+                if i == maxHashT-1: return
+                else: print("lista["+str(i)+"]:"+self.listas[i].printLista(maxList, self.listas[i].head, 0))
+
 #A partir de palavra, encontra todas as palavras a uma correçao de distancia
 def editWord(palavra):
     #Adicionar uma letra
@@ -77,18 +90,19 @@ def editWord(palavra):
 #Apaga a pontuaçao da palavra
 def delPont(palavra):
     hasChanged = True
-    while(hasChanged):
+    while hasChanged:
         hasChanged = False
         for char in palavra:
-            if(char in pontos):
+            if char in pontos:
                 hasChanged = True
                 palavra = palavra.replace(char, "")
                 break
 
     return palavra
 
-#Função de dispersão
+#Função de dispersão (folding)
 def hash(pal, size):
+
     sum = 0
     for char in pal:
         sum += ord(char)
@@ -100,6 +114,7 @@ def main():
     finalInputPals = []
     finalOutputPals = []
     distanciaErro1 = []
+    distanciaErro2 = []
 
     #Verifica os parametros
     try:
@@ -108,35 +123,48 @@ def main():
         print("Wrong number of arguments")
         sys.exit(0)
 
-    #Tabela de hash
-    tableSize = 4049 #Numero de palavras da lista é 3665
-    tabela = Tabela(tableSize)
+    #Tabela de hash e tamanho (tamanhos primos)
+    tamTabelaPals = 34 #Numero de palavras da lista é 3665
+    tabelaPals = Tabela(tamTabelaPals)
+    tamTabelaFreq = 1217 #Numero de frequencias é 1000
+    tabelaFreq = Tabela(tamTabelaFreq)
 
     #Localizaçao do ficheiro com as palavras do dicionario
-    wordsFilePath = "words/sortedWords_small.txt"
+    wordsFilePath = "words/en_sortedWords_small.txt"
+
+    #Adiciona as frequencias de algumas palavras numa tabela de dispersao
+
 
     #Adiciona as palavras do dicionario na tabela de dispersao
     with open(wordsFilePath, "r") as f:
         listaPalavras = [pal for pal in f.read().split()]
 
     for pal in listaPalavras:
-        tabela.add(hash(pal, tableSize), pal)
+        tabelaPals.add(hash(pal, tamTabelaPals), pal)
 
+
+    tabelaPals.printHash(True, 50, 50)
     #Le as palavras do input
     with open(fileInput, "r") as f:
         tempInputPals = [pal for pal in f.read().split()]
 
     #Remove pontuacao das palavras (se existir)
-    for pal in listaPalavras:
+    for pal in tempInputPals:
         finalInputPals.append(delPont(pal))
 
     #Verifica agora se ha erros ortograficos
     for pal in finalInputPals:
-        if(tabela.get(hash(pal, tableSize), pal) != ""): finalOutputPals.append(pal)
+        print(tabelaPals.get(hash(pal, tamTabelaPals), pal))
+        if(tabelaPals.get(hash(pal, tamTabelaPals), pal) != ""): finalOutputPals.append(pal)
         else:
             #Primeiro os erros de distancia 1
             distanciaErro1 = editWord(pal)
+            print(distanciaErro1)
+            #E os erros de distancia 2
+            #for pal in distanciaErro1:
+                #distanciaErro2 += editWord(pal)
+            #print(distanciaErro2, len(distanciaErro2))
 
-
+    print(finalOutputPals)
 if __name__ == "__main__":
     main()
